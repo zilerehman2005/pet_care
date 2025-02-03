1e400d38ecfbff4aee03ed9b6291687dd8be3195
zilerehman2005 <zillerehman004@gmail.com>
2025-02-03 19:25:53 +0500

    Create colors.dart

---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+
+import 'dart:ui';
+
+class AppColors{
+  static const Color redColor = Color(0xFFED6D4E);
+  static const Color pinkColor = Color(0xFFFD90AA);
+  static const Color violetColor = Color(0xFF4552CB);
+  static const Color lightPinkColor = Color(0xFFFFC4CD);
+  static const Color darkBlueColor = Color(0xFF2E1E7A);
+  static const Color orangeColor = Color(0xFFF1A852);
+  static const Color yellowColor = Color(0xFFFFCF6F);
+  static const Color palePinkColor = Color(0xFFFCECEA);
+  static const Color skyBlueColor = Color(0xFF4596EA);
+  static const Color  navyblueColor = Color(0xFF070821);
+  static const Color  blackColor = Color(0xFF000000);
+}
